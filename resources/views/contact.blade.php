0768173410d47e23aea84d305809cd86c6d28e8e
HP <dindanissa0715@gmail.com>
2024-09-24 16:03:27 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Contact</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            <a href="https://instagram.com/dndkhrn__">Instagram</a>
+        </li>
+        <li>
+            <a href="https://x.com/dndkhrn__">X</a>
+        </li>
+        <li>
+            <a href="https://youtube.com/@dndkhrn__">Youtube</a>
+        </li>
+    </ul>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Dinda Khairunnisa']);
+});
+
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
